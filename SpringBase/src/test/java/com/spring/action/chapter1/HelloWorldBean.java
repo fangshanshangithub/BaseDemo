123c132b02acadc4171d8be3f04478f729e9ed3a
fangss <643915247>
2017-04-01 14:41:36 +0800

    20170401--222

---
+++ SpringBase/src/test/java/com/spring/action/chapter1/HelloWorldBean.java
@@ -0,0 +1,7 @@
+package com.spring.action.chapter1;
+
+public class HelloWorldBean {
+	public String sayHello() {
+		return "Hello World";
+	}
+}
---
+++ SpringBase/src/test/java/com/test/spring/reflect/CarReflectTest.java
@@ -0,0 +1,14 @@
+package com.test.spring.reflect;
+
+import com.test.spring.Car;
+
+public class CarReflectTest {
+
+	/*public static Car initCarReflect() throws ClassNotFoundException {
+		ClassLoader loader = Thread.currentThread().getContextClassLoader();
+		Class clazz = loader.loadClass("Car");
+		
+	}*/
+	
+	
+}
