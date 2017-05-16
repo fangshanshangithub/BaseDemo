16f238b77d925154b73711a386536a16e17cf486
fangss <643915247>
2017-05-16 11:28:42 +0800

    单点登录  demo

---
+++ JavaBase/src/main/java/com/single/SingleDemo.java
@@ -0,0 +1,20 @@
+package com.single;
+
+public class SingleDemo {
+
+	public static void main(String[] args) {
+		
+		
+	}
+}
+
+class A {
+	public A a = new A();
+	
+	public  A() {
+		
+	}
+	
+	public static 
+	
+}
