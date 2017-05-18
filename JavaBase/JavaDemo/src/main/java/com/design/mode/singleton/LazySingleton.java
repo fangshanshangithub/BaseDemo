946577f18cedf0cefc8b42c49622cfafc5de1877
fangss <643915247>
2017-05-18 18:17:15 +0800

    发发发发发发发

--- JavaBase/JavaDemo/src/main/java/com/design/mode/singleton/LazySingleton.java
+++ JavaBase/JavaDemo/src/main/java/com/design/mode/singleton/LazySingleton.java
@@ -11,7 +11,7 @@ public class LazySingleton {
 	}
 	
 	// 没有初始化的LazySingleton 类的变量
-	private static LazySingleton single = null;
+	//private static LazySingleton single = null;
 	
 	//[非线程安全版本] 调用 get方法是先判断是否有 LazySingleton对象没有的话new一个
 	public static LazySingleton getSingle() {
@@ -21,13 +21,22 @@ public class LazySingleton {
 		return single;
 	}
 	
-	//[线程安全版本] 这个版本的实现牺牲了一定的效率，如果logObj已经初始化，那么其他线程还需要同步的进入getInstance方法，会造成效率的损失
+	 /*[线程安全版本] 这个版本的实现牺牲了一定的效率，如果logObj已经初始化，
+		那么其他线程还需要同步的进入getInstance方法，会造成效率的损失
+		缺点是：同步方法频繁调用时，效率略低。*/
 	/*public static synchronized LazySingleton getSingle() {
 		if(single == null) {
 			single = new LazySingleton();
 		}
 		return single;
 	}*/
+	
+	
+	/*
+	 * 最有方案
+	 */ 
+	private static volatile LazySingleton single;
+	
 	
 	public static void main(String[] args) {
 
