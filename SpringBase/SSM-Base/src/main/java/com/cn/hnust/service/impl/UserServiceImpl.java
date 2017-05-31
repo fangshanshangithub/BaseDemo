3b456ca701d1eb23e76b8dd0ebfeca16aca0fd8b
fangss <643915247>
2017-05-31 14:36:29 +0800

    萨芬的撒

---
+++ SpringBase/SSM-Base/src/main/java/com/cn/hnust/service/impl/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.cn.hnust.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cn.hnust.dao.UserMapper;
+import com.cn.hnust.pojo.User;
+import com.cn.hnust.service.IUserService;
+
+@Service("userService")
+public class UserServiceImpl implements IUserService {
+	
+	@Autowired
+	private UserMapper userDao;
+	
+	@Override
+	public User getUserById(int userId) {
+		return userDao.selectByPrimaryKey(userId);
+	}
+
+}
