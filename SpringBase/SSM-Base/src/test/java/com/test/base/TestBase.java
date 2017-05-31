633a0c7810061a337cd8ca9b6792d8eac668ada5
fangss <643915247>
2017-05-31 14:52:10 +0800

    SSM-Base  Test测试

---
+++ SpringBase/SSM-Base/src/test/java/com/test/base/TestBase.java
@@ -0,0 +1,11 @@
+package com.test.base;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class) //表示继承了SpringJUnit4ClassRunner类  
+@ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})//指定 Spring的相关配置
+public class TestBase {
+	
+}
