f43940c0fe9b99e7d9492ab22c0cbd8c21e020dd
fangss <643915247>
2017-06-01 17:05:13 +0800

    啊范德萨

--- SpringBase/SSM-Base/src/main/java/com/cn/hnust/controller/UserController.java
+++ SpringBase/SSM-Base/src/main/java/com/cn/hnust/controller/UserController.java
@@ -3,7 +3,6 @@ package com.cn.hnust.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
