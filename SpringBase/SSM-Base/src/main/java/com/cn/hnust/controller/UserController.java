7ae5de7d2a6877e5cc41c1252715fb40afd13b89
fangss <643915247>
2017-06-01 17:03:20 +0800

    萨芬的撒

---
+++ SpringBase/SSM-Base/src/main/java/com/cn/hnust/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.cn.hnust.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cn.hnust.pojo.User;
+import com.cn.hnust.service.IUserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@Autowired
+	private IUserService userService;
+	
+	@RequestMapping("/showUser")
+	public String toIndex(HttpServletRequest request,Model model) {
+		int userId = Integer.parseInt(request.getParameter("id"));  
+        User user = this.userService.getUserById(userId);  
+        model.addAttribute("user", user);  
+		return "showUser";
+	}
+	
+}
