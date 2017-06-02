565d0cbc5014a710ba985c18c59bc083684512de
fangss <643915247>
2017-06-02 14:51:55 +0800

    亲热武器拜拜

--- SpringBase/SSM-Base/src/test/java/com/test/base/TestBase.java
+++ SpringBase/SSM-Base/src/test/java/com/test/base/TestBase.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class) //表示继承了SpringJUnit4ClassRunner类  
 //@ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})//指定 Spring的相关配置
-@ContextConfiguration(locations = {"classpath:spring/spring-mybatis.xml"})
+@ContextConfiguration(locations = {"classpath:spring/application-spring*.xml"})
 public class TestBase {
 	
 }
