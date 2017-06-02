947ef480535d70e876af4dce592c68d0b907d283
fangss <643915247>
2017-06-02 14:50:09 +0800

    萨芬的萨芬多少

--- SpringBase/SSM-Base/src/test/java/com/test/base/TestBase.java
+++ SpringBase/SSM-Base/src/test/java/com/test/base/TestBase.java
@@ -5,7 +5,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class) //表示继承了SpringJUnit4ClassRunner类  
-@ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})//指定 Spring的相关配置
+//@ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})//指定 Spring的相关配置
+@ContextConfiguration(locations = {"classpath:spring/spring-mybatis.xml"})
 public class TestBase {
 	
 }
