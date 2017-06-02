e3a90080c312a157d8cc691b09d548374a246a4d
fangss <643915247>
2017-06-02 14:48:29 +0800

    萨芬的撒萨芬

--- SpringBase/SSM-Base/src/main/java/com/cn/hnust/controller/UserController.java
+++ SpringBase/SSM-Base/src/main/java/com/cn/hnust/controller/UserController.java
@@ -2,6 +2,7 @@ package com.cn.hnust.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,12 +15,16 @@ import com.cn.hnust.service.IUserService;
 @RequestMapping("/user")
 public class UserController {
 
+	// 日志功能
+	public static Logger logger = Logger.getLogger(UserController.class);
+	
 	@Autowired
 	private IUserService userService;
 	
 	@RequestMapping("/showUser")
 	public String toIndex(HttpServletRequest request,Model model) {
-		int userId = Integer.parseInt(request.getParameter("id"));  
+		int userId = Integer.parseInt(request.getParameter("id")); 
+		logger.info("********传过来的UserId = " + userId);
         User user = this.userService.getUserById(userId);  
         model.addAttribute("user", user);  
 		return "showUser";
