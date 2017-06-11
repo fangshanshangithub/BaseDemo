27d431eae904a8cf5e22efe3bcb7175f98c4780e
FangSS <FangSS@DESKTOP-268H1QN>
2017-06-11 22:57:55 +0800

    gfdsgfdsgfdsgfdsgfd

---
+++ JavaBase/JavaDemo/src/main/java/com/demo/collection/set/SetTest.java
@@ -0,0 +1,22 @@
+package com.demo.collection.set;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Set 不重复，无序 
+ * HashSet,TreeSet
+ * @author FangSS
+ */
+public class SetTest {
+
+	public static void main(String[] args) {
+		Collection collection1 = new HashSet();
+		Collection collection2 = new TreeSet();
+		Set set = new HashSet();
+	}
+
+	
+}
